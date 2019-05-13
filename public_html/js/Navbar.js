43b30ee71d07e0347c51678c78c25184c71a458d
Cody <r0766008@student.thomasmore.be>
2019-05-13 17:44:39 +0200

    Navbar extended hover JQuery

---
+++ public_html/js/Navbar.js
@@ -0,0 +1,26 @@
+$(function () {
+    var vandaag = new Date();
+    $('#time').text(vandaag.getFullYear());
+
+    var grootte = $('li').css('font-size');
+    var hover_grootte = parseInt(grootte) + 3;
+    var na_hover = parseInt(hover_grootte) - 3;
+    console.warn(hover_grootte);
+    $('li').hover(function () {
+        $(this).css('font-size', hover_grootte + "px");
+        if ($(this).attr("class") != "nav-item active") {
+            $(this).children("div").css('display', "block");
+            $(".nav-item.active").children("div").css('display', "none");
+            $(".nav-item.active").children("a").css('color', "rgba(0, 0, 0, 0.5)");
+        }
+    }, function () {
+        $(this).css('font-size', na_hover + "px");
+        if ($(this).attr("class") == "nav-item active") {
+            $(this).children("div").css('display', "block");
+        } else {
+            $(this).children("div").css('display', "none");
+            $(".nav-item.active").children("div").css('display', "block");
+            $(".nav-item.active").children("a").css('color', "rgba(0, 0, 0, 0.7)");
+        }
+    });
+});
